a43c8c6cc486ed21782b925265723b988a97d8fd
Matthias Mullie <git@mullie.eu>
2020-12-03 12:42:21 +0100

    Optimize for faster classification
    - Moved the rand() calculation into PHP instead of expecting
    MySQL to generate a random number for every row - should be
    much faster
    - Split submit & fetch into separate APIs, which will allow
    them to not be blocked on one another
    - Reworked submit flow in order to pre-fetch the next image
    so that it can be shown (if available) as soon as the exiting
    image is requested
    - Shuffled a few things around so that I can show Bing image
    search results in an iframe immediately on the same page
    (eliminating a manual step to cross-reference)

---
+++ public_html/fetch.php
@@ -0,0 +1,37 @@
+<?php
+
+$config = array_merge(
+	parse_ini_file( __DIR__ . '/../config.ini', true ),
+	parse_ini_file( __DIR__ . '/../replica.my.cnf', true )
+);
+$mysqli = new mysqli( $config['db']['host'], $config['client']['user'],
+	$config['client']['password'], $config['db']['dbname'] );
+if ( $mysqli->connect_error ) {
+	die('Connect Error (' . $mysqli->connect_errno . ') '
+	    . $mysqli->connect_error);
+}
+
+$maxId = $mysqli->query(
+	'select max(id)
+	from results_by_component'
+);
+if ( $maxId === false ) {
+	throw new Exception( 'No images exist' );
+}
+$maxId = intval( $maxId->fetch_assoc()['id'] );
+
+$result = $mysqli->query(
+	'select id, term, file_page, image_url
+	from results_by_component
+	where rating is null and skipped=0 and id >= '. $maxId .'
+	limit 1'
+);
+
+$mysqli->close();
+
+if ( $result === false ) {
+	throw new Exception( 'No image found' );
+}
+
+header('Content-Type: application/json');
+echo json_encode( $result->fetch_assoc() );
